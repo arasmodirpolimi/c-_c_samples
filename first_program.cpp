c383df58f297685a3468b51b16dced35ea7defe5
Arsalan Modirkhazeni <arsalan.modirkhazeni@mail.polimi.it>
2024-12-24 14:13:19 +0100

    Exploring differnces for cout methods

--- first_program.cpp
+++ first_program.cpp
@@ -2,6 +2,8 @@
 // new comments
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!1";
+    std::cout << "Hello, World!2" << std::endl;
     return 0;
 }
 
