b73833ae6e9089caee3854a677b73dcbe777c087
Arsalan Modirkhazeni <arsalan.modirkhazeni@mail.polimi.it>
2024-12-26 13:37:46 +0100

    Operator Overloading

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+using namespace std;
+
 // new comments
 
 int factorial(int n) {
@@ -6,12 +8,49 @@ int factorial(int n) {
     return n * factorial(n - 1);
 }
 
+// Complex number class
+class Complex {
+private:
+    double real;
+    double imag;
+
+public:
+    // Constructor
+    Complex(double r = 0, double i = 0) : real(r), imag(i) {}
+
+    // Overload the + operator
+    Complex operator+(const Complex other) const {
+        return Complex(real + other.real, imag + other.imag);
+    }
+
+    // Function to display the complex number
+    void display() const {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
 int main() {
-    int a = 4;
-    int b = 0;
-    b = factorial(a);
-    std::cout << "Factorial a = " << a << " is: " << b << std::endl;
+    Complex c1(2.5, 3.5); // 2.5 + 3.5i
+    Complex c2(1.5, 4.5); // 1.5 + 4.5i
+
+    // Use overloaded + operator
+    Complex c3 = c1 + c2;
+
+    // Display the result
+    cout << "Result of addition: ";
+    c3.display();
+
     return 0;
 }
+
+
+
+// int main() {
+//     int a = 4;
+//     int b = 0;
+//     b = factorial(a);
+//     std::cout << "Factorial a = " << a << " is: " << b << std::endl;
+//     return 0;
+// }
 
 
