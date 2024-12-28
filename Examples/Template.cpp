569064e9f253c97537f9d9f4c296c25220ff30fe
Arsalan Modirkhazeni <arsalan.modirkhazeni@mail.polimi.it>
2024-12-28 15:32:34 +0100

    Adding a template function

---
+++ Examples/Template.cpp
@@ -0,0 +1,24 @@
+/**
+ * Simple use of C++ templates.
+ * 
+ * @author
+ *   Wade Fagen-Ulmschneider <waf@illinois.edu>
+ */
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+template <typename T>
+T max(T a, T b) {
+  if (a > b) { return a; }
+  return b;
+}
+
+int main() {
+  cout << "max(3, 5): " << max(3, 5) << endl;
+  cout << "max('a', 'd'): " << max('a', 'd') << endl;
+  cout << "max(\"Hello\", \"World\"): " << max("Hello", "World") << endl;
+
+  return 0;
+}
